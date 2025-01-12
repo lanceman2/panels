183ea5a868e3412176e14937b7c3eafef8ab1e0d
Lance Arsenault <lanceiman@vt.edu>
2025-01-12 13:28:48 -0500

    Saving Work

---
+++ lib/surface.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <wayland-client.h>
+
+#include "../include/panels.h"
+
+#include "xdg-shell-protocol.h"
+#include "xdg-decoration-protocol.h"
+
+#include "debug.h"
+#include  "display.h"
+
+
+
