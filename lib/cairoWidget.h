3b25f38b347eb12d054b2bc019cca816a5a4ebb8
Lance Arsenault <lanceiman@vt.edu>
2025-02-16 19:04:34 -0500

    Saving Work

--- lib/cairoWidget.h
+++ lib/cairoWidget.h
@@ -1,14 +1,15 @@
 
 // button States
 //
-// Note: if it's not a toggle button it only has 4 usable states.
-// The toggle button adds another dimension 
+// Note: if it's not a toggle button it only has 4 usable states.  The
+// toggle button adds another dimension (call it selection), so it has 2 x
+// 4 = 8 states.  At what point is a button a selector that has N * 4
+// states?
 //
-// 
 #define NORMAL     0
-#define FOCUS      1
+#define HOVER      1
 #define PRESSED    2
-#define ACTIVE     3 // animation
+#define ACTIVE     3 // animation, released
 // TOGGLED is higher bits than the above states.
 #define TOGGLED    1 << 2 // on with this as normal
 #define NUM_STATES 
@@ -18,5 +19,5 @@ struct PnButton {
     struct PnWidget widget; // first inherit
 
     uint32_t state;
-    uint32_t colors[];
+    uint32_t colors[NUM_STATES];
 };
