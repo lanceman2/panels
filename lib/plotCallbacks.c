0d8b8638815f2290e629fb4a1d1cb16a130b962a
Lance Arsenault <lanceiman@vt.edu>
2025-04-02 09:15:08 -0400

    Saving Work

--- lib/plotCallbacks.c
+++ lib/plotCallbacks.c
@@ -18,30 +18,51 @@
 #include "plot.h"
 
 
+// We have just one mouse pointer object (and one enter/leave event cycle
+// at a time).  We can assume that using just one instance of mouse
+// pointer state variables is all that is needed; hence we can have these
+// as simple static variables (and not allocate something like them for
+// each plot).
+
+#define ENTERED   01
+#define PRESSED   02
+
+
+static uint32_t state;
+
 
 // widget callback functions specific to the plot:
+//
+bool enter(struct PnWidget *w,
+            uint32_t x, uint32_t y, struct PnPlot *p) {
+    DASSERT(!state);
+    state = ENTERED;
+
+WARN();
+    return true; // take focus
+}
+
+bool leave(struct PnWidget *w, struct PnPlot *p) {
+
+    DASSERT(state & ENTERED);
+    state = 0;
+WARN();
+    return true; // take focus
+}
+
 bool press(struct PnWidget *w,
             uint32_t which, int32_t x, int32_t y,
             struct PnPlot *p) {
     return true;
 }
 
-bool release(struct PnWidget *w,
-            uint32_t which, int32_t x, int32_t y,
+bool motion(struct PnWidget *w, int32_t x, int32_t y,
             struct PnPlot *p) {
     return true;
 }
 
-bool enter(struct PnWidget *w,
-            uint32_t x, uint32_t y, struct PnPlot *p) {
-    return true; // take focus
-}
-
-bool leave(struct PnWidget *w, struct PnPlot *p) {
-    return true; // take focus
-}
-
-bool motion(struct PnWidget *w, int32_t x, int32_t y,
+bool release(struct PnWidget *w,
+            uint32_t which, int32_t x, int32_t y,
             struct PnPlot *p) {
     return true;
 }
